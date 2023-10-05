9c1ff1f2e09ef248342cc60280e16b5cc32ad040
huy.tang2 <huy.tang2@netico.se>
2023-10-05 10:33:13 +0700

    feat: test component

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-/**
- * @format
- */
+import {Button} from './src';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+export {Button};
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import {Button} from './components/buttons/button.component';
+
+export {Button};
